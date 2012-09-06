a0ceb15b6c0eb58bd4eace02fa225f4a1cecd789
Scott Sanders <scott@jssjr.com>
2012-09-06 11:54:30 -0400

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Scott Sanders"
+maintainer_email "scott@jssjr.com"
 license          "All rights reserved"
 description      "Installs/Configures monit"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
